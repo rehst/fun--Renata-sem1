1da46ab64a4ef27b241714ed1415e5d35f506e03
rehst <107552363+rehst@users.noreply.github.com>
2024-06-07 08:38:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const pergunta1 = [
+    {
+        enunciado: "pergunta 1",
+        alternativas: [
+            "alternativa 1",
+            "alternativa 2"
+        ]
+    }
+    {
+        enunciado: "pergunta 2",
+        alternativas: [
+            "alternativa 1",
+            "alternativa 2"
+        ]
+    };
